60adb47d8388cdd58d82fd76c0470da60ced0017
liviaellen <38367623+liviaellen@users.noreply.github.com>
2025-04-07 22:08:27 -0700

    add feature processing script

--- src/create_features.py
+++ src/create_features.py
@@ -5,11 +5,16 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.feature_selection import SelectPercentile, f_classif
 import pickle
+import yaml
 
+# load params
+params = yaml.safe_load(open("params.yaml"))["features"]
+data_path = params["data_path"]
+use_scaler = params["scale_numeric"]
 
-train = pd.read_csv('course/Data/adult.data', header=None)
-test = pd.read_csv('course/Data/adult.test', header=None)
-
+# read data
+train = pd.read_csv(f'{data_path}/adult.data', header=None)
+test = pd.read_csv(f'{data_path}/adult.test', header=None)
 
 cols = ['age', 'workclass', 'fnlwgt', 'education', 'education-num',
         'marital-status', 'occupation', 'relationship', 'race', 'sex',
@@ -34,14 +39,17 @@ cat_cols = ['workclass', 'education', 'marital-status', 'occupation',
             'relationship', 'race', 'sex', 'native-country']
 
 
+# preprocessing pipeline with optional scaling
+num_pipe = StandardScaler() if use_scaler else 'passthrough'
 preprocessor = ColumnTransformer(
     transformers=[
-        ('num', StandardScaler(), num_cols),
+        ('num', num_pipe, num_cols),
         ('cat', OneHotEncoder(handle_unknown='ignore'), cat_cols)
     ])
 
 
-selector = SelectPercentile(f_classif, percentile=50)
+# feature selection with param
+selector = SelectPercentile(f_classif, percentile=params["percentile"])
 
 
 pipe = Pipeline([
@@ -54,9 +62,9 @@ X_train_processed = pipe.fit_transform(X_train, y_train)
 X_test_processed = pipe.transform(X_test)
 
 
-with open('course/Data/pipeline.pkl', 'wb') as f:
+with open(f'{data_path}/pipeline.pkl', 'wb') as f:
     pickle.dump(pipe, f)
 
 
-pd.DataFrame(X_train_processed).to_csv('course/Data/processed_train_data.csv', index=False)
-pd.DataFrame(X_test_processed).to_csv('course/Data/processed_test_data.csv', index=False)
+pd.DataFrame(X_train_processed).to_csv(f'{data_path}/processed_train_data.csv', index=False)
+pd.DataFrame(X_test_processed).to_csv(f'{data_path}/processed_test_data.csv', index=False)
